fcec4ac84d87b4607ca5c15a12a6ecb7faca28f9
SeongKookKIM <ksk8646@naver.com>
2023-11-27 11:07:50 +0900

    리액트 셋팅

---
+++ bnc/src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "./Locales/i18n";
+
+import "./Style.scss";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+reportWebVitals();
