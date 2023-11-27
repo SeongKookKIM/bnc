973edf08039bef0436869ae0f953ca952d6a1a00
SeongKookKIM <ksk8646@naver.com>
2023-11-27 13:54:14 +0900

    Footer 생성

--- bnc/src/App.tsx
+++ bnc/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ bnc/src/Components/Footer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-inner">
+        <h1>(주)비엔씨로지스틱</h1>
+        <div className="footer-section-box">
+          <span>
+            서울특별시 마포구 만리재로 14, 2102호(공덕동,르네상스타워)
+          </span>
+          <span>
+            대표 번호 +82-2-3272-5300 | 팩스 +82-2-3272-5455 | 대표 이메일
+            info@bncl.co.kr | 대표자 이원준 | 사업자 등록번호 105-86-21997
+          </span>
+        </div>
+        <div className="footer-section-box">
+          <span>
+            RM. 2102, Mallijae-ro 14, Mapo-gu, Seoul, Korea(Gongdeok-dong,
+            Renaissance Tower){" "}
+          </span>
+          <span>
+            Tel. +82-2-3272-5300 Fax. +82-2-3272-5455 E-mail. info@bncl.co.kr{" "}
+          </span>
+        </div>
+        <div className="footer-section-box">
+          <span className="copy">
+            BNC LOGISTICS Co.,Ltd. 2001&copy; All rights reserved.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
