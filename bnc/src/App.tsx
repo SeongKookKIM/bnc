15de55e226ca940f1696490f0aa1afe3463c89c3
SeongKookKIM <ksk8646@naver.com>
2023-11-27 12:55:20 +0900

    home 설정

--- bnc/src/App.tsx
+++ bnc/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Nav from "./Components/Nav";
 
 function App() {
-  return <div className="App">home</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ bnc/src/Pages/Home.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section className="home">
+      <div className="home-banner">
+        <div className="home-banner-text">
+          <strong>
+            Complex Global <br />
+            Logistics Simplified
+          </strong>
+          <span>
+            Knowledgeable in all aspects of maritime regulations,{" "}
+            <br className="mobile" /> we offer complete ocean solutions for your
+            supply chain.
+          </span>
+        </div>
+      </div>
+      <div className="inner">
+        <div className="home-about">
+          <h3>
+            Global Reach, Customized Solutions.<span>LEARN MORE{">"}</span>
+          </h3>
+          <div className="home-about-content-box">
+            <div className="home-about-content">
+              <p>
+                EMO Trans has been implementing customized global logistics
+                solutions for over 50 years. With 250 offices in 120 countries
+                at your fingertips, you can reach every corner of the globe
+                right from your front door. Nimble yet stable, we’re proud to
+                deliver the
+              </p>
+            </div>
+            <div className="home-about-content">
+              <p>
+                personalized service of a privately held company with the robust
+                infrastructure of a multi- national leader. Trust the expertise,
+                ingenuity and commitment of the EMO Trans team for creative,
+                customized logistics to solve your most complex cargo
+                challenges.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="home-link">
+          <div className="home-introduction">
+            <span>회사소개</span>
+          </div>
+          <div className="home-estimate">
+            <span>사업견적</span>
+          </div>
+          <div className="home-directions">
+            <span>오시는길</span>
+          </div>
+        </div>
+
+        <div className="home-partners">
+          <p>BNC LOGITICS와 함께하는 그룹</p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
